ff83c66d73eaf410625b7c5f4fc79ada0fede0c0
Jonas Holbech <jh@jonasholbech.dk>
2021-05-04 10:52:22 +0200

    cart play

--- src/MyBasket.js
+++ src/MyBasket.js
@@ -1,10 +1,28 @@
 export default function MyBasket({ basket }) {
+  const cart = [
+    { name: "Shirt", amount: 1 },
+    { name: "Pants", amount: 3 },
+    { name: "Socks", amount: 2 },
+  ];
+  const cartComponents = cart.map((item) => (
+    <CartItem name={item.name} amount={item.amount} />
+  ));
   return (
     <section className="MyBasket">
       <p>
         You have {basket.length} item{basket.length !== 1 ? "s" : ""} in your
         basket, totalling 0DKR
       </p>
+      <ul>{cartComponents}</ul>
     </section>
+  );
+}
+
+function CartItem(props) {
+  console.log(props);
+  return (
+    <li>
+      {props.amount} {props.name}
+    </li>
   );
 }
--- src/Product.js
+++ src/Product.js
@@ -1,18 +1,26 @@
 import { useState } from "react";
 export default function Product({ productdisplayname, soldOut, price, id }) {
-  const [count, setCount] = useState(0);
+  const [amount, setAmount] = useState(0);
 
-  function handleClick(evt) {
+  function handleClickUp(evt) {
     console.log(evt);
-    setCount((prevState) => {
+    setAmount((prevState) => {
       return prevState + 1;
+    });
+  }
+  function handleClickDown(evt) {
+    console.log(evt);
+    setAmount((prevState) => {
+      return prevState - 1;
     });
   }
   return (
     <article className={soldOut ? "Product soldout" : "Product"}>
       <h2>{productdisplayname}</h2>
       <p>{price}</p>
-      <button onClick={handleClick}>Read more {count}</button>
+      <button onClick={handleClickDown}> - </button>
+      {amount}
+      <button onClick={handleClickUp}> + </button>
     </article>
   );
 }
