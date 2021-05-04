193f65974d1660d4badd9e33172ba510cba60690
Jonas Holbech <jh@jonasholbech.dk>
2021-05-04 12:15:46 +0200

    cart state

--- src/MyBasket.js
+++ src/MyBasket.js
@@ -1,9 +1,20 @@
+import { useState } from "react";
 export default function MyBasket({ basket }) {
-  const cart = [
+  const [cart, setCart] = useState([
     { name: "Shirt", amount: 1 },
     { name: "Pants", amount: 3 },
     { name: "Socks", amount: 2 },
-  ];
+  ]);
+
+  function addToCart() {
+    setCart((nextCart) => [
+      ...nextCart,
+      {
+        name: "Shoes",
+        amount: 14,
+      },
+    ]);
+  }
 
   return (
     <section className="MyBasket">
@@ -11,6 +22,7 @@ export default function MyBasket({ basket }) {
         You have {basket.length} item{basket.length !== 1 ? "s" : ""} in your
         basket, totalling 0DKR
       </p>
+      <button onClick={addToCart}>Buy More</button>
       <ul>
         {cart.map((item) => (
           <CartItem name={item.name} amount={item.amount} key={item.name} />
