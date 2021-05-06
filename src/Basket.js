8e3a4f48e36aafc3d4f74c75d5f4256d9e78a385
Jonas Holbech <jh@jonasholbech.dk>
2021-05-06 12:43:49 +0200

    final

--- src/Basket.js
+++ src/Basket.js
@@ -4,7 +4,7 @@ export default function Basket(props) {
   return (
     <aside className="Basket">
       <MyBasket basket={props.basket} />
-      <CheckoutForm />
+      {props.basket.length > 0 ? <CheckoutForm /> : <p>But more stuff</p>}
     </aside>
   );
 }
--- src/ProductList.js
+++ src/ProductList.js
@@ -1,11 +1,14 @@
 import Product from "./Product";
 export default function ProductList(props) {
-  console.log(props);
   return (
     <main className="ProductList">
+      {props.products.length === 0 && <Loader />}
       {props.products.map((item) => (
         <Product addToBasket={props.addToBasket} {...item} key={item.id} />
       ))}
     </main>
   );
+}
+function Loader() {
+  return <p>Loading...</p>;
 }
